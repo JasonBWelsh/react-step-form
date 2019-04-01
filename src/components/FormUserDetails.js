ed0dfdb2656870ee2d2c71d6c620b42c80fafb66
Jason Welsh <jasonwelsh@C02WN1F3G8WN.local>
2019-04-01 15:06:58 -0400

    [react-step-form] FormUserDetails bascally complete

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -10,7 +10,7 @@ export class FormUserDetails extends Component {
     continue = e => {
         e.preventDefault();
         this.props.nextStep();
-    }
+    };
 
     render() {
         const { values, handleChange } = this.props;
@@ -20,13 +20,41 @@ export class FormUserDetails extends Component {
                 <AppBar title="Enter User Details" />
                 <TextField
                     hintText="Enter Your First Name"
-                    floatingLabelText="Name"
+                    floatingLabelText="First Name"
                     onChange={handleChange('firstName')}
+                    defaultValue={values.firstName}
+                />
+                <br />
+                <TextField
+                    hintText="Enter Your Last Name"
+                    floatingLabelText="Last Name"
+                    onChange={handleChange('lastName')}
+                    defaultValue={values.lastName} 
+                />
+                <br />
+                <TextField
+                    hintText="Enter Your Email"
+                    floatingLabelText="Email"
+                    onChange={handleChange('email')}
+                    defaultValue={values.email} 
+                />
+                <br />
+                <RaisedButton
+                    label="Continue"
+                    primary={true}
+                    style={StyleSheet.button}
+                    onClick={this.continue}
                 />
             </React.Fragment>
         </MuiThemeProvider>
         )
     }
 }
+
+const styles = {
+    button: {
+        margin: 15
+    }
+};
 
 export default FormUserDetails
